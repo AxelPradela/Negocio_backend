244130072b3fadef11ede98e0b101e1fdbbce281
smoshy15 <axelpradela03@gmail.com>
2026-01-24 11:31:58 -0300

    seteo de cosas 2

---
+++ Negocio_Back.API/Conttrollers/DishController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Negocio_Back.Conttrollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DishController : ControllerBase
+    {
+    }
+}
